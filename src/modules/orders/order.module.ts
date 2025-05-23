ec053a605c281226022f359b4bb18004abc6f670
Bhakti-Dhanani <bhagvatiben.wappnet@gmail.com>
2025-05-23 12:29:59 +0530

    create soket getway for the realitime notification

--- src/modules/orders/order.module.ts
+++ src/modules/orders/order.module.ts
@@ -4,10 +4,21 @@ import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order.entity';
 import { OrderItem } from '../order-items/order-item.entity';
+import { ProductModule } from '../products/product.module';
+import { NotificationModule } from '../notifications/notification.module';
+import { SocketModule } from '../../common/gateway/socket.module';
+import { NotificationService } from '../notifications/notification.service';
+import { Product } from '../products/product.entity';
+import { Warehouse } from '../warehouses/warehouse.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Product, Warehouse]),
+    ProductModule,
+    NotificationModule,
+    SocketModule,
+  ],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, NotificationService],
 })
 export class OrderModule {}
